fc5980134126a6eca4b9c080ec629d8ead65d76c
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-04 23:31:36 +0100

    created a function to add and revove active classes

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-const data = [
-    
-]
+const getQuestionHolder = document.querySelectorAll(".question-holder");
+const getWrapper = document.querySelectorAll('.wrapper')
+getQuestionHolder.forEach((currentItem) => {
+  currentItem.addEventListener("click", (event) => {
+    if (currentItem.classList.contains("active")) {
+      currentItem.classList.remove("active");
+    } else {
+      let getAlreadyAddedActiveClasses = document.querySelectorAll("active");
+
+      getAlreadyAddedActiveClasses.forEach((currentActiveItem) => {
+        currentActiveItem.classList.remove("active");
+      });
+      currentItem.classList.add("active");
+    }
+  });
+});
+
+
+console.log(getWrapper);
