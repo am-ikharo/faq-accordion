1e769f3f108d664f7f0a0685e544fe9f7e15040b
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-05 12:18:51 +0100

    rewriting the js

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-const getWrapper = document.querySelectorAll('.question-holder');
+const getquestionHolder = document.querySelectorAll('.question-holder');
 
 
-getWrapper.forEach(currentItem => {
-  currentItem.addEventListener('click', (event) => {
-    if(currentItem.classList.contains('active')) {
-      currentItem.classList.remove('active')
-    } else {
-      let getAlreadyAddedClasses = document.querySelectorAll('.active')
-
-      getAlreadyAddedClasses.forEach(currentActiveItem => {
-        currentActiveItem.classList.remove('active')
-      })
-      currentItem.classList.add('active')
-    }
-  })
-})
