f7938e2ebaf3e1773a1f60a7192b49de0c333370
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-05 11:58:38 +0100

    formatted the js code

--- main.js
+++ main.js
@@ -1,28 +1,16 @@
 const getQuestionHolder = document.querySelectorAll(".question-holder");
-const image1 = './assets/images/icon-plus.svg'
-const image2 = './assets/images/icon-minus.svg'
-
-
-
 
 getQuestionHolder.forEach((currentItem) => {
   currentItem.addEventListener("click", (event) => {
     if (currentItem.classList.contains("active")) {
       currentItem.classList.remove("active");
-      
     } else {
       let getAlreadyAddedActiveClasses = document.querySelectorAll(".active");
 
       getAlreadyAddedActiveClasses.forEach((currentActiveItem) => {
         currentActiveItem.classList.remove("active");
-        
       });
       currentItem.classList.add("active");
-      
-      
     }
   });
 });
-
-
-console.log(getWrapper);
