13071477d4a247f216aee254f1d23045382afde5
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-07-05 12:33:17 +0100

    rewrite

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const getquestionHolder = document.querySelectorAll('.question-holder');
 
 
+getquestionHolder.forEach(currentItem => {
+  currentItem.addEventListener('click', (event) => {
+    if(currentItem.classList.contains('active')){
+      currentItem.classList.remove('active');
+    }else {
+      let getAlreadyAddedClassList = document.querySelectorAll('.active');
+      getAlreadyAddedClassList.forEach(currentActiveClasses => {
+        currentActiveClasses.classList.remove('active')
+      })
+      currentItem.classList.add('active')
+    }
+  })
+})
